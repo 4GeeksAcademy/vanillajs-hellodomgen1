d4a09d6f7c9f37b43cdfaffe76103e13a5029bae
CamBombarda <osoriocamila@live.com>
2024-11-06 00:21:53 +0000

    dngok

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// arrays with the words
+let pronoun = ["theonly", "my", "our"];
+let adj = ["great", "crazy", "giant"];
+let noun = ["cheerleader", "cat", "dragon", "master", "universe"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// generating the domains
+for (let c = 0; c < pronoun.length; c++) {
+  for (let o = 0; o < adj.length; o++) {
+    for (let p = 0; p < noun.length; p++) {
+      console.log(pronoun[c] + adj[o] + noun[p] + ".com");
+    }
+  }
+}
